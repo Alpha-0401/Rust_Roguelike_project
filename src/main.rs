009b56170db1451fdc5af8b6d5bd77a215bc7f94
Alpha-0401 <victor.chen004@gmail.com>
2026-04-06 21:52:43 +0200

    Mouvements ajoutés

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,46 @@
 use bracket_lib::prelude::*;
 
-//Définition de l'état du jeu
-struct State {}
+//Définition de l'état du jeu (sauvegarde de coordonnées)
+struct State {
+    player_x: i32,
+    player_y: i32,
+    }
 
 //Boucle principale
 impl GameState for State {
+    //fonction pour mettre à jour
     fn tick(&mut self, ctx: &mut BTerm) {
+        
+        //Lire clavier
+        if let Some(key) = ctx.key{
+        println!("Tecla presionada: {:?}", key);
+            match key{
+                // Mouvement vers la gauche (Q/A)
+                VirtualKeyCode::Left => self.player_x -= 1,
+                // Mouvement vers la droite (D)
+                VirtualKeyCode::Right => self.player_x += 1,
+                // Mouvement vers le haut (Z/W)
+                VirtualKeyCode::Up => self.player_y -= 1,
+                // Mouvement vers le bas (S)
+                VirtualKeyCode::Down => self.player_y += 1,
+                // Ignorer le rest
+                _ => {}
+            }
+        }
+        
         //Nettoyer écran à chaque frame
         ctx.cls();
         
         ctx.print(1,1,"Roguelike Test");
         
         //Player
-        ctx.print_color(40,25, RGB::named(YELLOW), RGB::named(BLACK), "@");
+        ctx.print_color(
+            self.player_x,
+            self.player_y,
+            RGB::named(YELLOW),
+            RGB::named(BLACK),
+            "@"
+        );
     }
 }
 
@@ -22,7 +50,12 @@ fn main() -> BError {
     let context = BTermBuilder::simple80x50()
         .with_title("First Roguelike")
         .build()?;
-    //Initialiser
-    let gs = State{};
+         
+    //Initialiser (Joueur au millieu)
+    let gs = State{
+        player_x: 40,
+        player_y: 25,
+    };
+    
     main_loop(context,gs)
 }
